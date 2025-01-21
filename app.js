4fe137da3c2344f9384583bb5e1d730f9238f678
Zaksk <zakariamar.99@gmail.com>
2025-01-21 09:57:01 +0000

    Add cors to project

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+
 const logger = require("./logger")
+
+
+app.use(cors())
+app.use(express.json())
 app.use(logger) // if it was logger() it would tell the server to run it as soon as it starts and the fact that we are not using an endpoint it means to run it on every endpoint
 
-app.use(express.json())
 const fruitsRouter = require("./routes/fruits")
 
 
